cce7b4c93c3d9c2f5ca15ae5c35b8d98438fa68c
Winston Rast <wrast@ecsteam.com>
2017-03-09 11:53:16 -0700

    fix path reference

--- ci/create.sh
+++ ci/create.sh
@@ -2,5 +2,5 @@
 
 ls -l iaas-smoke-test-release
 ls -l cna-iaas-smoke-test
-cp -r iaas-smoke-test-release/IaasSmokeTest_linux cna-iaas-smoke-test/config.json smoke-test/.
+cp -r iaas-smoke-test-release/IaaSSmokeTest_linux cna-iaas-smoke-test/config.json smoke-test/.
 ls -l smoke-test
