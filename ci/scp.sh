c3f9f87e5b8214cc02c78826d4b70fe698097c0e
Winston Rast <wrast@ecsteam.com>
2017-03-09 14:13:24 -0700

    add scp

---
+++ ci/scp.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/expect
+
+#SMOKETEST_USER=ubuntu
+#SMOKETEST_HOST=172.28.10.234
+#SMOKETEST_PATH=.
+#SMOKETEST_PASSWORD=welcome1
+
+#spawn scp -r scp.yml $SMOKETEST_USER@$SMOKETEST_HOST:$SMOKETEST_PATH
+#expect "password:"
+#send "$SMOKETEST_PASSWORD\n"
+#interact
+
+spawn scp -r smoke-test ubuntu@172.28.10.234:.
+expect "password:"
+send "welcome1\n"
+interact
