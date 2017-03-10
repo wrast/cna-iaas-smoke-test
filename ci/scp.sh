2394e94bbf0bb41e84ef1dc701d1ef6cb074b720
Winston Rast <wrast@ecsteam.com>
2017-03-10 12:22:57 -0700

    remove args usage

--- ci/scp.sh
+++ ci/scp.sh
@@ -10,17 +10,17 @@
 #send "$SMOKETEST_PASSWORD\n"
 #interact
 
-set host [lindex $argv 0]
-set user [lindex $argv 1]
-set password [lindex $argv 2]
-set remotePath [lindex $argv 3]
+# set host [lindex $argv 0]
+# set user [lindex $argv 1]
+# set password [lindex $argv 2]
+# set remotePath [lindex $argv 3]
 
-spawn scp -r smoke-test $user@$host:$remotePath
+spawn scp -r smoke-test $host_user@$host_name:$host_path
 
 expect "Are you sure you want to continue connecting (yes/no)?"
 send "yes\n"
 
 expect "password:"
-send "$password\n"
+send "$host_password\n"
 
 interact
