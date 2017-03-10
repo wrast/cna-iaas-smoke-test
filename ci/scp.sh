b751466e9ac7cce4a76c107be2666a69aa92813e
Winston Rast <wrast@ecsteam.com>
2017-03-10 12:28:35 -0700

    expect env var test

--- ci/scp.sh
+++ ci/scp.sh
@@ -10,17 +10,17 @@
 #send "$SMOKETEST_PASSWORD\n"
 #interact
 
-# set host [lindex $argv 0]
+set host $env(host_name)
 # set user [lindex $argv 1]
 # set password [lindex $argv 2]
 # set remotePath [lindex $argv 3]
 
-spawn scp -r smoke-test $host_user@$host_name:$host_path
+spawn scp -r smoke-test $env(host_user)@$host:$env(host_path)
 
 expect "Are you sure you want to continue connecting (yes/no)?"
 send "yes\n"
 
 expect "password:"
-send "$host_password\n"
+send "$env(host_password)\n"
 
 interact
