b66b2e2bdaa1451ee8627bd679ef1204f2859807
Winston Rast <wrast@ecsteam.com>
2017-03-13 14:31:47 -0600

    add test script

--- ci/run_host.sh
+++ ci/run_host.sh
@@ -2,3 +2,4 @@
 
 cna-iaas-smoke-test/ci/create.sh
 cna-iaas-smoke-test/ci/scp.sh
+cna-iaas-smoke-test/ci/test.sh
---
+++ ci/test.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/expect
+
+set host $env(host_name)
+set user $env(host_user)
+set password $env(host_password)
+set remotePath $env(host_path)
+
+spawn ssh $user@$host
+
+#expect "Are you sure you want to continue connecting (yes/no)?"
+#send "yes\n"
+
+expect "password:"
+send "$password\n"
+
+cd smoke-test
+echo "Ready to run"
+exit
+
+interact
